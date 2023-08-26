8f282569c101849e9541d0b547d6563ff26ec768
Николай <130489187+kolyan994@users.noreply.github.com>
2023-08-26 20:42:01 +0300

    Update search.py

--- search.py
+++ search.py
@@ -11,28 +11,34 @@ service_key = os.getenv('service_key')
 user_token = os.getenv('user_token')
 vk2 = vk_api.VkApi(token=user_token).get_api()
 
-
-def get_user_info(user_id): # возвращает данные о текущем пользователе по ID
+def get_user_info(user_id):
 	user_info = vk.users.get(user_id=user_id, fields='bdate, city, sex')
+	print(user_info)
 	return user_info[0]
 
-
-def search_users(user_info, offset): # Ищет подходящего человека. Необходимо передать, а после изменить offset
-	city = user_info['city']['id']
+def search_users(user_info, offset, city=1, age_from=18, age_to=30):
+	try:
+		city = user_info['city']['id']
+	except:
+		pass
+	try:
+		age = user_info['bdate'][-4:]
+		age_from = 2023 - 5 - int(age)
+		age_to = 2023 + 5 - int(age)
+	except:
+		pass
 	if user_info['sex'] == 1:
 		sex = 2
 	else:
 		sex = 1
-	age = user_info['bdate'][-4:]
-	age_from = 2023 - 5 - int(age)
-	age_to = 2023 + 5 - int(age)
 
+	print(city, age_from, age_to)
 	users_list = vk2.users.search(city=city, sex=sex, age_from=age_from, age_to=age_to, has_photo=1, count=1, offset=offset)
 
 	return users_list['items'][0]['id']
 
 
-def get_info(user_id): # Возвращает нужные данные и фото переданного человека
+def get_info(user_id):
 	photo_data = vk2.photos.get(extended=1, album_id='profile', photo_sizes=1)['items']
 	photo_data = sorted(photo_data, key=lambda x: x['likes']['count'], reverse=True)[:3]
 	user_data = vk2.users.get(user_id=user_id, fields='city, bdate')
@@ -52,7 +58,7 @@ def get_info(user_id): # Возвращает нужные данные и фо�
 
 if __name__ == '__main__':
 
-	user_info = get_user_info(42145010)
+	user_info = get_user_info(571405905)
 
 	offset = 0
 	while True:
