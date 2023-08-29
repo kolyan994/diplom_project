6cb1c65820728f0bbc2cb7784bdbcc544479ecb5
Leontev VS <leontev.vitaliy.s@gmail.com>
2023-08-29 13:11:27 +0300

    add auth

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     bot = VKinderBot()
     bot_thread = Thread(target=bot.start_bot)
     server_thread = Thread(target=start_server)
-    # server_thread.start()
+    server_thread.start()
     bot_thread.start()
